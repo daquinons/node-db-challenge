69422222bc6e43434f17e14b0aa411f329d4893d
David Quiñones <daquinons@users.noreply.github.com>
2019-07-19 10:49:55 +0200

    Add Contexts to database

---
+++ data/seeds/02-addContexts.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('Contexts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('Contexts').insert([
+        {id: 1, name: 'At home'},
+        {id: 2, name: 'At the gym'}
+      ]);
+    });
+};
