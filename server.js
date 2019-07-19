61149844ae63aa8032cd6c9189aeaa952756dc07
David Quiñones <daquinons@users.noreply.github.com>
2019-07-19 09:39:43 +0200

    Install helmet and morgan

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('combined'));
 
 module.exports = server;
